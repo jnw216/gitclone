6270fff6f97922130c82c90bb2d2f6b0b5f8745e
jonathannwinters <jonathannwinters@ubuntu17.04>
2017-06-24 23:56:47 -0400

    removing methods

--- app.py
+++ app.py
@@ -8,15 +8,6 @@ app = Flask(__name__)
 def index():
     return "GitClone!"
 
-@app.route('/push')
-def push():
-    return "PUSH"
-
-@app.route('/pull')
-def push():
-    return "PULL"
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
