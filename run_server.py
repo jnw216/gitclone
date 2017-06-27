8de4d26050a798f8f69631445d1c741ebcbdff46
jonathannwinters <jonathannwinters@ubuntu17.04>
2017-06-27 00:05:27 -0400

    organizing

--- run_server.py
+++ run_server.py
@@ -1,6 +1,10 @@
+from flask import Flask
 from gitclone import app
+app = Flas(__name__)
+from views import *
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 #if __name__ == "__main__":
 #    port = int(os.environ.get("PORT",5000))
