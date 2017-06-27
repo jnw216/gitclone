65035add4217bd0d15b79a70d0180688f60b3fc0
jonathannwinters <jonathannwinters@ubuntu17.04>
2017-06-27 01:00:17 -0400

    scratch

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello:
-    return "Hello World!"
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#db = SQLAlchemy(app)
+#migrate = Migrate(app,db)
 
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/')
+def index():
+    # PRINT ALL COMMITS  
+    return "GitClone!    <br>" 
+
+
+
+@app.route('/push')
+def push():
+    return "pushing file"
+
+@app.route('/pull')
+def pull():
+    return "pulling file"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
