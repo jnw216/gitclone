c09213419a464e2f70619377aa7ee83a8ba82698
jonathannwinters <jonathannwinters@ubuntu17.04>
2017-06-28 19:10:38 -0400

    removing error

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
-from models import *
+#from models import *
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 migrate = Migrate(app, db)
